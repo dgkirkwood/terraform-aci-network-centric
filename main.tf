ebd22bf3c3e6f8b6a96639ca7a59c1d98e7f4d20
dgkirkwood <daniel.g.kirkwood@gmail.com>
2020-05-16 21:38:28 +1000

    alter for cloud demo

--- main.tf
+++ main.tf
@@ -30,21 +30,3 @@ module vlan {
   tenant_name     = "foo"
   gateway_address = "10.10.1.1/24"
 }
-
-module vlan2 {
-  source          = "./vlan"
-  tenant_name     = "foo2"
-  gateway_address = "10.10.2.1/24"
-  unicast_route   = "yes"
-}
-
-module vlan3 {
-  source          = "./vlan"
-  tenant_name     = "foo3"
-  gateway_address = "10.10.3.1/24"
-}
-module vlan4 {
-  source          = "./vlan"
-  tenant_name     = "foo4"
-  gateway_address = "10.10.4.1/24"
-}
--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "aci_url" {
   type = string
+  default = "https://sandboxapicdc.cisco.com"
 }
 
 variable "aci_username" {
   type = string
+  default = "admin"
 }
 
 variable "aci_password" {
   type = string
+  default = "ciscopsdt"
 }
